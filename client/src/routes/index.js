53731f174d98d7dc1d85ec60544311949302f04a
James White <YOUR_EMAIL@DOMAIN.COMjames.ewhitejr@yahoo.com>
2019-12-11 09:56:15 -0500

    finished resolving merge conflicts

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -3,25 +3,12 @@ import { Route, Switch } from 'react-router-dom'
 import SignIn from '../screens/SignIn'
 import SignOut from '../screens/SignOut'
 import SignUp from '../screens/SignUp'
-<<<<<<< HEAD
-=======
 import Home from '../screens/Home'
 import Items from '../screens/Items'
->>>>>>> bba077653099de789c5bcd1bafe7ca9313ca65e9
 import Login from '../screens/Login'
 import AuthenticatedRoute from './AuthenticatedRoute'
 const Routes = ({ setUser, clearUser }) => (
     <Switch>
-<<<<<<< HEAD
-        {/* <Route
-        path="/sign-in"
-        render={props => <SignIn {...props} setUser={setUser} />}
-        />
-        <Route
-        path="/sign-up"
-        render={props => <SignUp {...props} setUser={setUser}/>}
-        /> */}
-=======
         <Route
         exact
         path="/"
@@ -31,21 +18,12 @@ const Routes = ({ setUser, clearUser }) => (
         path="/login"
         render={props => <Login {...props} setUser={setUser} />}
         />
->>>>>>> bba077653099de789c5bcd1bafe7ca9313ca65e9
         <Route
         exact
         path="/sign-out"
         render={props => <SignOut {...props} clearUser={clearUser}/>}
         />
-<<<<<<< HEAD
-        <AuthenticatedRoute
-        exact
-        path="/login"
-        render= {<Login />} />
-
-=======
         
->>>>>>> bba077653099de789c5bcd1bafe7ca9313ca65e9
     </Switch>
 )
 
--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -2,34 +2,14 @@ import React, { Component } from 'react'
 import Header from './Header'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
-<<<<<<< HEAD
-import SignOut from './SignOut'
-
-const Login = () => {
-    return(
-    <>
-=======
 import '../styles/login.css'
 
 class Login extends Component {
     render () {
         return (
->>>>>>> bba077653099de789c5bcd1bafe7ca9313ca65e9
     <>
     <div className="login-bar">
     <SignIn />
-<<<<<<< HEAD
-    </>
-    <>
-    <SignOut />
-    </>
-    </>
-    )
-}
-
-export default Login
-
-=======
     <SignUp />
     </div>
     </>
@@ -37,4 +17,3 @@ export default Login
 } 
 
 export default Login
->>>>>>> bba077653099de789c5bcd1bafe7ca9313ca65e9
