a7c17536d443a0f49b270f013ee39e7dde0982e1
James White <YOUR_EMAIL@DOMAIN.COMjames.ewhitejr@yahoo.com>
2019-12-11 21:40:22 -0500

    working on functionality for color change

--- client/src/shared/Nav.jsx
+++ client/src/shared/Nav.jsx
@@ -6,8 +6,8 @@ const Nav = () => (
 	<nav>
         <NavLink to="/">ORNATE OPTICAL</NavLink>
         <ul className="main-bar">
-        <li>MEN</li>
-        <li>WOMEN</li>
+        <NavLink to='/glasses-men'><li>MEN</li></NavLink>
+        <NavLink to='/glasses-women'><li>WOMEN</li></NavLink>
         <li>SERVICES</li>
         </ul>
         <ul className="login-bar">
